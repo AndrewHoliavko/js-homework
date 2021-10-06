01419c9f0fa5760f0b77874a2703d4e6b8d981dc
Web <web@it.step>
2021-10-06 21:27:11 +0300

    loops lesson 2

--- consp.js
+++ consp.js
@@ -1,3 +1,3 @@
 //ОБЩИИ ОКНСПЕКТ
-https://github.com/fashion-prepod/homework // Владислав Викторович Git hub
-https://join.skype.com/BK9BQF-qkWspT // Skype Влыдислава Викторовича
+// https://github.com/fashion-prepod/homework // Владислав Викторович Git hub
+// https://join.skype.com/BK9BQF-qkWspT // Skype Влыдислава Викторовича
--- loops/script.js
+++ loops/script.js
@@ -1,2 +1,43 @@
 // ПРИМЕРЫ
 
+// ответ на задачу 1
+
+// for(let i = 1; i <= 4;i++){
+//     for(let j =1;j <= 4;j++){
+//         if (j===3 && i===3){
+//             continue;
+//         }
+//         console.log(`${i}.${j}`);
+//     }
+// }
+
+//модернизированный ответ на задачу 1
+// let enteredNum = prompt('enter ur number');
+// for(let i = 1; i <= enteredNum;i++){
+//     for(let j =1;j <= enteredNum;j++){
+//         console.log(`${i}.${j}`);
+//     }
+// }
+
+//ответ на 2 задачу
+
+let enteredNum = +prompt('enter number');
+let summ = 0;
+for (let i = 0 ;enteredNum >= i;i++){
+    summ = summ + i;
+    console.log(enteredNum);
+    
+}
+console.log(summ);
+
+//2 ответ на 2 задачу
+
+// let enteredNum = +prompt('enter number');
+// let summ = enteredNum;
+// for (;enteredNum >= 1;enteredNum--){
+//     summ = summ + enteredNum - 1;
+//     console.log(enteredNum);
+    
+// }
+// console.log(summ);
+
