bdb00cd9c77cd2c160c8bf4894ba5ce1464a9603
Web <web@it.step>
2021-10-10 11:51:03 +0300

    1.1 arrays lesson

--- arrays/script.js
+++ arrays/script.js
@@ -1,9 +1,18 @@
-let team = ['Jhon','David','Cristiano','Andrew'];
+let team = ['Jhon', 'David', 'Cristiano', 'Andrew'];
 console.log(team[1]); //David
+team[0] = 'Olga'
+team[4] = 'Yan'
+delete team[1]; //останеться элемет но его внутренность будет удалена
+console.log(team[team.length - 1]);//всегда выведет последний елемент массива
+team[team.length] = 'Olya'
 
 console.log([1,2,3][1]);//2
 
 console.log([1,2,1+3][2])//4
 
 console.log([1,2,1+3][2-1])//2
+
+let arr = [prompt(), prompt(), prompt(), prompt()]
+console.log(arr);
+
 
