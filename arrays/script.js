02fbe8e24e953cfaf71c073a6293297287513af3
Darin376 <Sleeper2012@yandex.ru>
2021-10-13 20:10:49 +0300

    methods of arrys

--- arrays/script.js
+++ arrays/script.js
@@ -1,3 +1,6 @@
+let arr = [3, 345, 675, 44]
+arr.push(200, 60, 77); 
+
 let team = ['Jhon', 'David', 'Cristiano', 'Andrew'];
 console.log(team[1]); //David
 team[0] = 'Olga'
@@ -14,5 +17,4 @@ console.log([1,2,1+3][2-1])//2
 
 let arr = [prompt(), prompt(), prompt(), prompt()]
 console.log(arr);
-
 
