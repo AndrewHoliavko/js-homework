cc69b617ef48f661cdc005116373fe85c0230a0e
Darin376 <Sleeper2012@yandex.ru>
2021-10-13 21:21:46 +0300

    exersises arrays

--- arrays/script.js
+++ arrays/script.js
@@ -1,20 +1,29 @@
-let arr = [3, 345, 675, 44]
-arr.push(200, 60, 77); 
+const arr = [1,2,3,4,-5,6,7,8,-9,-10];
 
-let team = ['Jhon', 'David', 'Cristiano', 'Andrew'];
-console.log(team[1]); //David
-team[0] = 'Olga'
-team[4] = 'Yan'
-delete team[1]; //останеться элемет но его внутренность будет удалена
-console.log(team[team.length - 1]);//всегда выведет последний елемент массива
-team[team.length] = 'Olya'
+// console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]+arr[7]+arr[8]+arr[9]);
 
-console.log([1,2,3][1]);//2
+// let answer = 0;
+// let j = 0
+// for (let i = 0; i<10;i++){
+//     if(arr[j]%2===0){
+//         answer = answer + arr[j]
+//     }
+//     j++
+// }
+// console.log(answer);
 
-console.log([1,2,1+3][2])//4
+let arrCopy = arr.slice();
+let j = 0
+for(let i = 0;i < 10;i++){
+    if (arrCopy[j]>0){
+        arrCopy[j] = - + arrCopy[j]
+    }
+    else {
+        arrCopy[j] = arrCopy[j] * -1
+    }
+    j++
+}
+console.log(arrCopy);
 
-console.log([1,2,1+3][2-1])//2
 
-let arr = [prompt(), prompt(), prompt(), prompt()]
-console.log(arr);
 
