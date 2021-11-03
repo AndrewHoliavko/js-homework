286d1a24c6a50ff51606711e0d66891fed530ab6
Andrew <holiavkoandrew@gmail.com>
2021-11-03 21:23:49 +0300

    function methods and another

--- objects/consp.js
+++ objects/consp.js
@@ -26,3 +26,19 @@ const keys = Object.keys(person) // запись объекта в массив
 for (let key in person){ // key имя элемента 
     console.log( person[key] );
 }
+
+
+
+
+
+// const popit = {
+//     blue: 234,
+//     red: 214,
+//     orange: 23,
+//     purple: 124
+// }
+// let sum = 0;
+// for (let key in popit){ 
+//     sum += popit[key]   
+// }
+// console.log( sum );
--- objects/script.js
+++ objects/script.js
@@ -1,12 +1,2 @@
-const popit = {
-    blue: 234,
-    red: 214,
-    orange: 23,
-    purple: 124
-}
-// let sum = 0;
-// for (let key in popit){ 
-//     sum += popit[key]   
-// }
-// console.log( sum );
+
 
